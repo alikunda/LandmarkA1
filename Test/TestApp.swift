a07f08073c2808ef3d6ed38446fa912c19265ca7
Mohammed Ali Kunda <mkund01@jaguar.tamu.edu>
2022-02-17 17:40:49 -0600

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Mohammed on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
